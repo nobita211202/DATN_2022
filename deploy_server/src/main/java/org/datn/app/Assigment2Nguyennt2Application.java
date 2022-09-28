40ea6656063726df4bf6f8bf5a866d1d21c3bfe7
Your Name <you@example.com>
2022-09-28 11:04:35 +0700

    tholv

---
+++ deploy_server/src/main/java/org/datn/app/Assigment2Nguyennt2Application.java
@@ -0,0 +1,13 @@
+package org.datn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assigment2Nguyennt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assigment2Nguyennt2Application.class, args);
+	}
+
+}
