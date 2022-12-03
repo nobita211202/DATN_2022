3a6e827c87d93b19699c11afa117f261415cda4e
Your Name <you@example.com>
2022-12-03 19:55:08 +0700

    update database tholv

--- datn_fpoly_2022/src/main/java/org/datn/controller/OrderDetailController.java
+++ datn_fpoly_2022/src/main/java/org/datn/controller/OrderDetailController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.sql.SQLException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/order-detail")
@@ -13,7 +14,12 @@ public class OrderDetailController {
     @Autowired
     OrderDetailService orderDetailService;
     @PostMapping("/order/{userId}")
-    public void orderByUserIdAndMultiCourse(@PathVariable Long userId,@RequestBody Long[] multiCourse) throws SQLException, ClassNotFoundException {
-        orderDetailService.orderByUserIdAndMultiCourse(userId,multiCourse);
+    public Map orderByUserIdAndMultiCourse(@PathVariable Long userId, @RequestBody Long[] multiCourse) throws SQLException, ClassNotFoundException {
+       try {
+              orderDetailService.orderByUserIdAndMultiCourse(userId,multiCourse);
+              return Map.of("message","success","status",200);
+       }catch (Exception e){
+           return Map.of("message","fail","status",500);
+       }
     }
 }
