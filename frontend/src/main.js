334a1a07aba309fd7181ed76ffb81ced6ecaf296
Your Name <you@example.com>
2022-12-15 16:12:30 +0700

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -26,24 +26,9 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 // Don't warn about using the dev version of Vue in development.
-Vue.config.productionTip = process.env.NODE_ENV === 'production'
-
-// If running inside Cypress...
-if (process.env.VUE_APP_TEST === 'e2e') {
-  // Ensure tests fail when Vue emits an error.
-  Vue.config.errorHandler = window.Cypress.cy.onUncaughtException
-}
 export const bus = new Vue()
 const app = new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount('#app')
-
-// If running e2e tests...
-if (process.env.VUE_APP_TEST === 'e2e') {
-  // Attach the app to the window, which can be useful
-  // for manually setting state in Cypress commands
-  // such as `cy.logIn()`.
-  window.__app__ = app
-}
