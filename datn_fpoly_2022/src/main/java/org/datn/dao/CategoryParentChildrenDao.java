7a1710460970a238d378b4283d3978114f1aaace
hoangnd2022002 <115718027+hoangnd2022002@users.noreply.github.com>
2023-01-10 14:32:17 +0700

    hoang

---
+++ datn_fpoly_2022/src/main/java/org/datn/dao/CategoryParentChildrenDao.java
@@ -0,0 +1,13 @@
+package org.datn.dao;
+
+import org.datn.entity.CategoryParentChildren;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CategoryParentChildrenDao extends JpaRepository<CategoryParentChildren,Integer> {
+    @Query("select c from CategoryParentChildren c where c.parent is null")
+    List<CategoryParentChildren> getParent();
+}
---
+++ datn_fpoly_2022/src/main/java/org/datn/entity/CategoryParentChildren.java
@@ -0,0 +1,68 @@
+package org.datn.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryParentChildren {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private Long id;
+
+    @OneToMany(mappedBy = "parent")
+    List<Category> children;
+
+    @Column(name = "parent_id")
+    private Long parent;
+
+    @ManyToOne()
+    @JsonIgnore
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @Column(name = "name_")
+    @Type(type = "org.hibernate.type.TextType")
+    private String name;
+
+    @Column(name = "interest")
+    private Long interest;
+
+    @Column(name = "like_")
+    private Long like;
+
+    @Column(name = "created")
+    private Date created;
+
+    @Size(max = 255)
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "creator")
+    @Type(type = "org.hibernate.type.TextType")
+    private String creator;
+
+    @Column(name = "modified")
+    private Date modified;
+
+    @Column(name = "modifier")
+    @Type(type = "org.hibernate.type.TextType")
+    private String modifier;
+
+    @Column(name = "status")
+    private Short status;
+
+
+}
