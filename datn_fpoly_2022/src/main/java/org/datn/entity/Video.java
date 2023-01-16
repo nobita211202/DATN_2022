9e5b638d37a2731e312247fff7bb5f674f0503ac
Your Name <you@example.com>
2023-01-16 16:23:32 +0700

    tholv-update-cart

---
+++ datn_fpoly_2022/src/main/java/org/datn/entity/Video.java
@@ -0,0 +1,28 @@
+package org.datn.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name = "videos")
+@Data@NoArgsConstructor@AllArgsConstructor
+public class Video implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "video_id")
+    private Long id;
+    private String name;
+    private String url;
+    private String describe;
+    private String created;
+    private String creator;
+    private String modified;
+    private String modifier;
+    private Integer status;
+}
